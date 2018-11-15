e95246869cb1312d27592617c99e098db2ced92a
Absoas <abso8787@naver.com>
2018-11-16 01:22:10 +0900

    20181116

--- webStudy01/WebContent/WEB-INF/views/fileBrowser.jsp
+++ webStudy01/WebContent/WEB-INF/views/fileBrowser.jsp
@@ -1,3 +1,7 @@
+<%@page import="kr.or.ddit.web.ServerFileBrowser"%>
+<%@page import="org.apache.commons.lang3.StringUtils"%>
+<%@page import="java.util.Map.Entry"%>
+<%@page import="java.util.Map"%>
 <%@page import="java.io.File"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
@@ -6,10 +10,15 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
-<script>
+	<%
+		Map<String, String>  files =  ServerFileBrowser.fileList;
+		File[] liFiles = (File[])request.getAttribute("liFiles");
+	%>
+<script type="text/javascript">
 	function showList(command){
-	    var form = document.show;
+	    var form=document.listForm;
 	    form.command.value = command;
+		sd = command;
 	    form.submit();
 	 }
 </script>
@@ -17,10 +26,35 @@
 <body>
 <!-- 파일들의 목록이 directory라면  -->
 <!-- parameter 설계 해서 2번을 클릭했다면 2번을 클릭햇다라는 정보를 보냄 -->
-
-	<form name="show" action="<%=request.getContextPath()%>">
-
+	<form name="listForm">
+	<%
+		
+		String format = "<ul onclick=\"javascript:showList('%s')\">%s";	
+		String liFormat = "<li>%s</li>";
+		for(Entry<String,String> entry : files.entrySet()){
+			out.println(String.format(format,  entry.getKey(), entry.getKey())); 
+			if(liFiles != null){
+				for(File li : liFiles){
+					if(li.getParentFile().getName().equals(entry.getKey())){
+						if(li.isDirectory()){
+							for(Entry<String,String> entry1 : ServerFileBrowser.fileList.entrySet()){
+								ServerFileBrowser.fileList.put(li.getName(), li.getAbsolutePath());
+								System.out.println(li.getName());
+								out.println("</ul>");
+								out.println(String.format(format, li.getName(), li.getName()));
+								out.println("</ul>");
+								continue;
+							}
+						}
+						out.println(String.format(liFormat,li.getName()));
+					}
+				}
+			}
+			
+			out.println("</ul>");
+		}
+	%>
+		<input type="hidden" name="command" value = "">
 	</form>
-	
 </body>
 </html>
--- webStudy01/src/kr/or/ddit/web/ServerFileBrowser.java
+++ webStudy01/src/kr/or/ddit/web/ServerFileBrowser.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,21 +19,44 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.jasper.tagplugins.jstl.core.Out;
 
+
 @WebServlet("/fileBrowser.do")
 public class ServerFileBrowser extends HttpServlet {
+
+	public static Map<String, String> initFileList = new LinkedHashMap<>();
+	public static Map<String, String> fileList = new LinkedHashMap<>();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
-		File webContent = new File(getServletContext().getRealPath(""));
+		
+		File webContent = new File(getServletContext().getRealPath("/"));
+		File[] files = webContent.listFiles();
 
-		for (final File fileEntry : webContent.listFiles()) {
-			if (!fileEntry.isDirectory()) {
-				
-			} 
+		
+		for (File file : files) {
+			if (file.isDirectory()) {
+				initFileList.put(file.getName(), file.getAbsolutePath());
+				fileList.put(file.getName(), file.getAbsolutePath());
+			}
+		}
+         
+		String command =  req.getParameter("command");
+//		System.out.println(fileList.get(command));
+//		File liList2 = new File(fileList.get(command));
+//		File liFiles2[] = null;
+//		liFiles2 = liList2.listFiles();
+//		
+//		System.out.println(liFiles2[0].getName());
+//		
+		if(command!=null) {
+			File liList = new File(fileList.get(command));
+			File liFiles[] = null;
+			liFiles = liList.listFiles();
+			req.setAttribute("liFiles", liFiles);
 		}
 		
-//		String view = "/WEB-INF/views/fileBrowser.jsp";
-//		RequestDispatcher rd  = req.getRequestDispatcher(view);
-//		rd.forward(req, resp);
+		String view = "/WEB-INF/views/fileBrowser.jsp";
+		RequestDispatcher rd  = req.getRequestDispatcher(view);
+		rd.forward(req, resp);
 	}
 }
